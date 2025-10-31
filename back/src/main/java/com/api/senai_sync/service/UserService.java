f79039193f794c0fae84aa133c47ca93303f51a4
davimoura27 <dange6783@gmail.com>
2025-10-31 14:08:52 -0300

    pequena alteração no service de register

--- back/src/main/java/com/api/senai_sync/service/UserService.java
+++ back/src/main/java/com/api/senai_sync/service/UserService.java
@@ -2,9 +2,8 @@ package com.api.senai_sync.service;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
 import com.api.senai_sync.controller.dto.UserResponseDto;
 import com.api.senai_sync.entity.User;
 import com.api.senai_sync.exceptions.EmailException;
@@ -16,14 +15,16 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     private ModelMapper modelMapper = new ModelMapper();
 
     public UserResponseDto saveUser(User user) {
         if (userRepository.existsByEmail(user.getEmail())) {
             throw new EmailException();
         }
-        String encoderPassword = new BCryptPasswordEncoder().encode(user.getPassword());
-        user.setPassword(encoderPassword);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
         return modelMapper.map(user, UserResponseDto.class);
     }
